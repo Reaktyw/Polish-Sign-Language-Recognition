ccbf8f5e2d95f0287893d2e04761a868ddeb745e
Reaktyw <132897153+Reaktyw@users.noreply.github.com>
2025-04-22 21:13:51 +0200

    Poprawka
    Nie ważne

--- Rozpoznawanie_Liter/Nasze/.ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-folder = "Rozpoznawanie_Liter/Nasze/dane/a.npy"
-a1 = np.load(folder)
-
-folder = "data/ą/0.npy"
-a2 = np.load(folder)
-
-print(f"Porównanie naszego i ichniego: \n {a1}, \n \n {a2}")
-print("cos tam")
